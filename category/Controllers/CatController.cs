721e0545c08a58d52c9a3b96981b3d4b8fcb9dd9
LENOVO <LENOVO@LAPTOP-COLQ3BIK>
2021-08-11 00:48:02 +0530

    Add project files.

---
+++ category/Controllers/CatController.cs
@@ -0,0 +1,71 @@
+﻿
+using category.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace category.Controllers
+{
+    public class CatController : Controller
+    {
+        // GET: Cat
+        db_testEntities dbobj = new db_testEntities();
+        public ActionResult Cat(Category1 obj)
+        {
+            if (obj != null)
+                return View(obj);
+            else
+                return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Catpro(Category1 model)
+        {
+            Category1 obj = new Category1();
+            if (ModelState.IsValid)
+            {
+                obj.CategoryId = model.CategoryId;
+                obj.CategoryName = model.CategoryName;
+
+                if (model.CategoryId == 0)
+                {
+                    dbobj.Category1.Add(obj);
+                    dbobj.SaveChanges();
+                }
+                else
+                {
+                    dbobj.Entry(obj).State = EntityState.Modified;
+                    dbobj.SaveChanges();
+                }
+
+            }
+            ModelState.Clear();
+
+            return View("Cat");
+        }
+       
+        public ActionResult CatList()
+        {
+            var res = dbobj.Category1.ToList();
+            return View(res);
+        }
+        public ActionResult Delete(int category_id)
+        {
+            var res = dbobj.Category1.Where(x => x.CategoryId == category_id).First();
+            dbobj.Category1.Remove(res);
+            dbobj.SaveChanges();
+            var list = dbobj.Category1.ToList();
+
+            return View("CatList", list);
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ productlist/Controllers/productlistController.cs
@@ -0,0 +1,90 @@
+﻿using productlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace productlist.Controllers
+{
+    public class productlistController : Controller
+    {
+        db_testEntities dbobj = new db_testEntities();
+
+        // GET: productlist
+        public ActionResult Index()
+        {
+            var res = dbobj.ProductList1.OrderBy.Where(x => x.ProductName).ToList();
+            return View(res);
+        }
+
+        // GET: productlist/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+       
+
+        // POST: productlist/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: productlist/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: productlist/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: productlist/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: productlist/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
